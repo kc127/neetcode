737fb6e232c159682b79a60b1f8267e3dd6782f4
Kanchan Chauhan <kanchan827@gmail.com>
2023-07-13 17:41:40 -0400

    efficient solution

--- codingDrills/searchInSortedMatrix.js
+++ codingDrills/searchInSortedMatrix.js
@@ -1,3 +1,5 @@
+// non-effecient solution
+
 var searchMatrix = function(matrix, target) {
   for (let r = 0; r < matrix.length; r++) {
     for (let c = 0; c < matrix[0].length; c++) {
@@ -6,3 +8,23 @@ var searchMatrix = function(matrix, target) {
   }
   return false;
 };
+
+// efficient solution
+
+// O(r + c) where r is the number of rows and c is the number of columns
+function searchMatrix(matrix, target) {
+  const rows = matrix.length;
+  const columns = matrix[0].length;
+
+  let r = 0, c = columns - 1;
+  while (r < rows && c >= 0) {
+    const val = matrix[r][c];
+    if (val === target) return true;
+    if (val < target) {
+      r++;
+    } else {
+      c--;
+    }
+  }
+  return false;
+}
