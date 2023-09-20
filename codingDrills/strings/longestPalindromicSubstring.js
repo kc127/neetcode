3b0c6813249539ef2c13293f4ff98f47b83649e5
Kanchan Chauhan <kanchan827@gmail.com>
2023-09-20 15:28:10 -0400

    longest palindromic substring

--- codingDrills/strings/longestPalindromicSubstring.js
+++ codingDrills/strings/longestPalindromicSubstring.js
@@ -17,3 +17,38 @@ Given a string: "aefez" // returns "efe"
 longest palindromic substring
 
 */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  let longest = [0,0];
+
+  for (let i = 0; i < s.length; i++) {
+      let oddPal = checkPal(i-1, i + 1, s);
+      let evenPal = checkPal(i, i + 1, s);
+
+      let oddPalLen = oddPal[1] - oddPal[0];
+      let evenPalLen = evenPal[1] - evenPal[0];
+
+      if (oddPalLen > evenPalLen) {
+          if (longest[1] - longest[0] < oddPalLen) {
+              longest = oddPal;
+          }
+      } else {
+          if (longest[1] - longest[0] < evenPalLen) {
+              longest = evenPal;
+          }
+      }
+  }
+  return s.slice(longest[0], longest[1]);
+};
+
+var checkPal = function(i, j, str) {
+  while (i >= 0 && i < str.length && j >= 0 && j < str.length && str[i] === str[j]) {
+      i--;
+      j++;
+  }
+  return [i+1, j];
+}
