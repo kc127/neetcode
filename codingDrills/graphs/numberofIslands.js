a1d0a5b9ee0bd90185fd3ac6f62598d500fdc3df
Kanchan Chauhan <kanchan827@gmail.com>
2023-10-06 12:32:48 -0400

    both bfs and dfs solution

--- codingDrills/graphs/numberofIslands.js
+++ codingDrills/graphs/numberofIslands.js
@@ -1,3 +1,5 @@
+/* 1. DFS Solution */
+
 var numIslands = function(grid) {
   let islands = 0;
 
@@ -25,3 +27,49 @@ var numIslands = function(grid) {
   }
   return islands;
 };
+
+/* BFS Solution */
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+
+  let islands = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+          if (grid[i][j] === "1") {
+              bfs(grid, i, j);
+              islands++;
+          }
+      }
+  }
+  return islands;
+};
+
+var bfs = function(grid, i, j) {
+  let [ROWS, COLS] = [grid.length, grid[0].length];
+
+  let directions = [
+      [-1, 0],
+      [1, 0],
+      [0, -1],
+      [0, 1],
+  ];
+
+  let queue = [[i,j]];
+  grid[i][j] = "0";
+  while (queue.length > 0) {
+      let [cr, cc] = queue.shift();
+
+      for (let [dr,dc] of directions) {
+          let [nr, nc] = [cr + dr, cc + dc];
+          if (!(nr < 0 || nr >= ROWS || nc < 0 || nc >= COLS || grid[nr][nc] === "0")) {
+              queue.push([nr,nc]);
+              grid[nr][nc] = "0";
+          }
+      }
+  }
+}
