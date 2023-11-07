c49e1ded4a099801968d42468d12061dd339f0a9
Kanchan Chauhan <kanchan827@gmail.com>
2023-11-06 20:43:30 -0500

    detect a cycle

--- codingDrills/graphs/olympics_graphQ2.js
+++ codingDrills/graphs/olympics_graphQ2.js
@@ -24,114 +24,128 @@ There is a cycle!
 */
 function solution(adjacency) {
 
-  let adjList = {};
-  for (let i = 0; i < adjacency.length; i++) {
-      adjList[i] = [];
-      let neighbors = adjacency[i];
-      for (let j = 0; j < neighbors.length; j++) {
-          let neighbor = neighbors[j];
-          if (neighbor === true) {
-              adjList[i].push(j);
-          }
-      }
-  }
-  console.log(adjList)
-  let visited = new Set();
-  function dfs(node) {
-      if (visited.has(node)) {
-          return true;
-      }
-      if (!adjList[node] || adjList[node].length === 0) {
-          return false;
-      }
+    let adjList = {};
+    for (let i = 0; i < adjacency.length; i++) {
+        adjList[i] = [];
+        let neighbors = adjacency[i];
+        for (let j = 0; j < neighbors.length; j++) {
+            let neighbor = neighbors[j];
+            if (neighbor === true) {
+                adjList[i].push(j);
+            }
+        }
+    }
 
-      visited.add(node);
+    function dfsHelper(node, path, visited) {
+      if (path.has(node)) {
+        return true;
+      }
+      if (visited.has(node)) {
+        return true;
+      }
+      path.add(node);
+
       for (let neighbor of adjList[node]) {
-          if (visited.has(neighbor) || dfs(neighbor)) {
-              return true;
-          } else if (!visited.has(neighbor) || !dfs(neighbor)) {
-              continue;
-          }
+        if (dfsHelper(neighbor, path, visited)) {
+          return true;
+        }
+        visited.add(neighbor);
       }
       return false;
+    }
+
+    for (let node in adjList) {
+      if (dfsHelper(node, new Set(), new Set())) {
+        return true;
+      }
+    }
+    return false;
   }
-  let bool = false;
-  for (let vertex in adjList) {
-      bool = dfs(vertex);
-  }
-  return bool;
-}
-
-/*
-
-function dfs
-  if (node === target) return true;
-
-  dfs(node.left)
-  dfs(node.right)
-
-
-convert adjacency matrix to list
-traverse the list using DFS
-if visited has already visited node
-return true
-
-
-*/
 
 
 
-// test cases
-let adjacency = [[false,true,false,false,false],
-[false,false,false,true,false],
-[false,false,false,false,false],
-[false,false,false,false,false],
-[false,true,false,false,false]]
 
-adjacency: [[false]]
-adjacency: [[true]]
+  // test cases
 
-adjacency:
-[[false,true],
- [false,false]]
+  let adjacency = [[false,true,false,false,false],
+                   [false,false,false,true,false],
+                   [false,false,false,false,false],
+                   [false,false,false,false,false],
+                   [false,true,false,false,false]]
 
- adjacency:
-[[false,true],
- [true,false]]
+  console.log(solution(adjacency), " expect false"); // false
 
- adjacency:
-[[false,false,false,true,true],
- [true,false,true,true,false],
- [false,true,true,false,true],
- [false,true,true,true,false],
- [false,true,false,false,true]]
+  /*
+        0-1-3
+          |
+          4
 
+  */
 
- adjacency:
-[[false,true,false,false,false],
- [false,false,false,true,false],
- [false,false,false,false,false],
- [false,false,false,false,false],
- [false,true,false,false,false]]
+  adjacency = [[false]]
+  console.log(solution(adjacency), " expect false"); // false
 
+  adjacency = [[true]]
+  console.log(solution(adjacency), " expect true");
 
- adjacency:
-[[false,false,false,false,true],
- [true,false,true,false,false],
- [false,false,false,true,false],
- [false,false,false,true,false],
- [false,true,false,false,false]]
+  adjacency =
+  [[false,true],
+   [false,false]]
 
+  console.log(solution(adjacency), " expect false");
 
- adjacency:
-[[false,false,false,true,false],
- [false,false,true,false,false],
- [true,false,false,true,false],
- [true,true,false,false,false],
- [true,true,false,true,false]]
+   adjacency =
+  [[false,true],
+   [true,false]]
 
- adjacency:
-[[false,true,false,false],
- [false,false,false,false],
- [false,false,false,true],
- [false,false,true,false]]
+  console.log(solution(adjacency), " expect true");
+
+   adjacency =
+  [[false,false,false,true,true],
+   [true,false,true,true,false],
+   [false,true,true,false,true],
+   [false,true,true,true,false],
+   [false,true,false,false,true]]
+
+  /*
+      1-2
+      |\
+      0-3
+      |
+      4
+
+  */
+
+  console.log(solution(adjacency));
+
+   adjacency =
+  [[false,true,false,false,false],
+   [false,false,false,true,false],
+   [false,false,false,false,false],
+   [false,false,false,false,false],
+   [false,true,false,false,false]]
+
+   console.log(solution(adjacency));
+
+   adjacency =
+  [[false,false,false,false,true],
+   [true,false,true,false,false],
+   [false,false,false,true,false],
+   [false,false,false,true,false],
+   [false,true,false,false,false]]
+   console.log(solution(adjacency));
+
+   adjacency =
+  [[false,false,false,true,false],
+   [false,false,true,false,false],
+   [true,false,false,true,false],
+   [true,true,false,false,false],
+   [true,true,false,true,false]]
+   console.log(solution(adjacency));
+
+   adjacency =
+  [[false,true,false,false],
+   [false,false,false,false],
+   [false,false,false,true],
+   [false,false,true,false]]
+   console.log(solution(adjacency));
