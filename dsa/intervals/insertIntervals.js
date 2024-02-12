bb0466750fb6e14f9f3191bfd6aec1fe81a76e3a
Kanchan Chauhan <kanchan827@gmail.com>
2024-02-12 14:19:02 -0500

    insert intervals

--- dsa/intervals/insertIntervals.js
+++ dsa/intervals/insertIntervals.js
@@ -20,3 +20,33 @@ var insert = function(intervals, newInterval) {
   let merged = left.concat(right);
   return merged;
 };
+
+/** O(n) time and O(n) space */
+
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+    let result = [];
+    let i = 0;
+    let n = intervals.length;
+    while (i < n && intervals[i][1] < newInterval[0]) {
+        result.push(intervals[i]);
+        i++;
+    }
+
+    while (i < n && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+        newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+        i++;
+    }
+    result.push(newInterval);
+
+    while (i < n) {
+        result.push(intervals[i]);
+        i++;
+    }
+    return result;
+};
